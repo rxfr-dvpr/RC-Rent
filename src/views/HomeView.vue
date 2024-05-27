37cef2e8a168fb9d95fc8c7778ff9aabd2c45ce2
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-05-27 23:35:00 +0500

    project updated and sass added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="text-box">
+    <h1>RC-Rent Soon !</h1>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'Home Page',
+  components: {
+    Nav
+  }
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+.text-box {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  h1 {
+    color: var(--main-yellow);
+  }
+}
+
+</style>
