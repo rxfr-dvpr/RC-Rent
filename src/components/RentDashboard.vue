4371b1c4c64c566f93cff6bdeef99dbeef8e9e46
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-06-07 23:00:32 +0500

    select option added to dashboard

--- src/components/RentDashboard.vue
+++ src/components/RentDashboard.vue
@@ -4,6 +4,17 @@
         <div class="row">
             <div class="dashboard-panel">
                 <h2 class="dashboard-title all-title-clr" v-html="store.title"></h2>
+
+                <div class="dashboard-options">
+                    <select name="" id="" v-for="(select, idx) in store.selects" :key="idx" :class="`all-select select-${idx + 1}`">
+                        <option :value="select.optionName" selected disabled class="all-option">{{ select.optionName }}</option>
+
+                        <option :value="option" v-for="(option, idd) in select.options" class="all-option"
+                        :key="idd">{{ option }}</option>
+                    </select>
+                </div>
+
+                <button class="dashboard-panel-btn all-btn">{{ store.btnTxt }}</button>
             </div>
 
             <img :src="store.img.bg" alt="" class="dashboard-bg">
@@ -48,6 +59,10 @@ export default {
         border: solid 1px #888888;
         border-radius: 20px;
         padding: 60px 0 35px;
+
+        &-btn {
+            padding: 8px 70px;
+        }
     }
 
     .dashboard-bg {
@@ -58,6 +73,35 @@ export default {
         left: 50%;
         transform: translate(-50%, -50%);
         z-index: -1;
+    }
+
+    .dashboard-options {
+        max-width: 640px;
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        row-gap: 30px;
+        column-gap: 80px;
+        justify-content: center;
+
+        .all-select {
+            max-width: 43.5%;
+            width: 100%;
+            color: #0C0C0B;
+            outline: none;
+            padding-left: 55px;
+            font-size: 24px;
+            cursor: pointer;
+
+            option {
+                color: #0C0C0B;
+                cursor: pointer;
+            }
+
+            &.select-1 {
+                max-width: 100% !important; 
+            }
+        }
     }
 }
 
--- src/stores/dashboardStore.js
+++ src/stores/dashboardStore.js
@@ -3,13 +3,53 @@ import { defineStore } from "pinia";
 export const dashboardStore = defineStore('Dashboard Store', {
     state: () => ({
         title: '<span class="yellow">Аренда автомобиля</span>',
-        options: [
+        selects: [
             {
-                optionName: 'Машина'
-            }
+                optionName: 'Машина',
+                options: [
+                    'BMW i325',
+                    'BMW 320',
+                    'BMW Z4',
+                    'BMW 420D',
+                    'Mercedes С180',
+                    'Mercedes Viano',
+                    'KIA K5',
+                    'KIA RIO',
+                    'Volkswagen Touareg',
+                ]
+            },
+            {
+                optionName: 'Машина',
+                options: [
+                    'BMW i325',
+                    'BMW 320',
+                    'BMW Z4',
+                    'BMW 420D',
+                    'Mercedes С180',
+                    'Mercedes Viano',
+                    'KIA K5',
+                    'KIA RIO',
+                    'Volkswagen Touareg',
+                ]
+            },
+            {
+                optionName: 'Машина',
+                options: [
+                    'BMW i325',
+                    'BMW 320',
+                    'BMW Z4',
+                    'BMW 420D',
+                    'Mercedes С180',
+                    'Mercedes Viano',
+                    'KIA K5',
+                    'KIA RIO',
+                    'Volkswagen Touareg',
+                ]
+            },
         ],
         img: {
             bg: 'https://firebasestorage.googleapis.com/v0/b/rc-rent-7f09a.appspot.com/o/Rent%2Frent-bg.png?alt=media&token=9b9e2924-a8eb-4918-8afb-daf70aa3a818'
-        }
+        },
+        btnTxt: 'Далее'
     })
 })
