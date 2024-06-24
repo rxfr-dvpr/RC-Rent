7289b28a5f27ad5dc24803a650df62814ad73298
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-06-24 19:30:31 +0500

    navigation fully adaptived to mobile version

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,15 +6,17 @@
                 <img :src="logoImg" alt="" class="nav-logo-img">
             </router-link>
 
-            <div class="nav__wrapper">
-                <ul class="nav__list">
+            <div class="nav__wrapper" :class="{'opened': navOpened}">
+                <ul class="nav__list" @click.self="navOpened = false">
                     <li class="nav__list-item" v-for="(link, idx) in store.links" :key="idx">
-                        <a :href="link.url" class="nav__list-link">{{ link.name }}</a>
+                        <a :href="link.url" class="nav__list-link" @click="navOpened = false">{{ link.name }}</a>
                     </li>
                 </ul>
             </div>
 
             <a href="tel:+998998173066" class="contact-num">+998 99 817 30 66</a>
+
+            <button class="nav-mb-btn" @click="navOpened = !navOpened"><i class="fal fa-bars"></i></button>
         </div>
     </div>
   </nav>
@@ -29,7 +31,8 @@ export default {
         return {
             logoImg: 'https://firebasestorage.googleapis.com/v0/b/rc-rent-7f09a.appspot.com/o/logo.png?alt=media&token=c4d5e6aa-2467-425b-b5ff-145d3c437601',
             store: navStore(),
-            bgBlur: false
+            bgBlur: false,
+            navOpened: false
         }
     },
     mounted() {
@@ -59,6 +62,14 @@ export default {
         justify-content: space-between;
         align-items: center;
         gap: 20px;
+    }
+
+    &-logo {
+        max-width: max-content;
+
+        img {
+            width: 100%;
+        }
     }
 
     &__wrapper {
@@ -96,10 +107,23 @@ export default {
         padding: 20px 0 !important;
         backdrop-filter: blur(20px);
     }
+
+    &-mb-btn {
+        display: none;
+        width: 35px;
+        height: 35px;
+        border: 0;
+        background: transparent;
+        justify-content: center;
+        align-items: center;
+        font-size: 25px;
+    }
 }
 
 @media (max-width: 992px) {
     .nav {
+
+        padding: 25px 0;
         &__wrapper {
             max-width: max-content;
             position: absolute;
@@ -108,6 +132,12 @@ export default {
             height: 100vh;
             padding: 0 15px;
             background: var(--main-yellow);
+            transform: translateX(-120%);
+            transition: .7s;
+
+            &.opened {
+                transform: translateX(0%);
+            }
         }
 
         &__list {
@@ -118,8 +148,22 @@ export default {
             &-link {
                 color: var(--main-dark);
                 font-weight: 600;
+
+                &:hover {
+                    color: var(--main-white) !important;
+                }
             }
         }
+
+        &-mb-btn {
+            display: flex;
+        }
+    }
+}
+
+@media (max-width: 556px) {
+    .nav-logo {
+        max-width: 80px !important;
     }
 }
 
